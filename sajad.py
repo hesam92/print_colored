c803719be92f4dfcf7a25bd627f81d5565944bdc
hesam92 <hessam1371.sh@gmail.com>
2018-12-25 12:08:18 -0800

    primary

---
+++ sajad.py
@@ -0,0 +1,10 @@
+from termcolor import colored, cprint
+
+a='sajad galaaaa'
+b='amir gal bashin ye'
+c='miladin gotoona ghoorban'
+# text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+# print(text)
+cprint(a, 'red')
+cprint(b, 'green')
+cprint(c, 'blue')
